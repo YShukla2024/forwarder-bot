6e9825cbf0a308b3ef6dcfb2655555da37e396e5
Vishal Rawat <vishalrawat@synergetics-consulting.com>
2026-04-22 11:46:57 +0530

    update source chats and improvements

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running"
+
+def run_web():
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+threading.Thread(target=run_web).start()
+
 import os
 import re
 import asyncio
